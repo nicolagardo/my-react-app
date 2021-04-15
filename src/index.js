fe4745b8e2c50f4881b46cce33cbe1b915882f9c
nicolagardo <nicolagardo@gmail.com>
2021-04-14 21:07:05 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+const user = {
+  firstName: 'Nicolas',
+  lastName: 'Lagardo'
+};
 
+const element = (
+ // <h1>
+   // Hello, {formatName(user)}!
+  //</h1>
+  <h1>{getGreeting(user)}</h1>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
